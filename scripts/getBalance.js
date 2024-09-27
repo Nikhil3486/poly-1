3c5d5511c8f16e6952cf61f1cb5d9cc715472793
Nikhil3486 <89239512+Nikhil3486@users.noreply.github.com>
2024-09-28 01:58:01 +0530

    Update getBalance.js

--- scripts/getBalance.js
+++ scripts/getBalance.js
@@ -1,9 +1,9 @@
 const hre = require("hardhat");
-const tokenContractJSON = require("../artifacts/contracts/Gurdwara.sol/Gurdwara.json");
+const tokenContractJSON = require("../artifacts/contracts/millioner.sol/millioner.json");
 
-const tokenAddress = "0x78EDe6348F07E5499340b709d4e325963Dc651d5"; // place your erc20 contract address here
+const tokenAddress = "0x0d7805DDDc91e61274815E9A7327f1C6c7fD9480"; // place your erc20 contract address here
 const tokenABI = tokenContractJSON.abi;
-const walletAddress = "0x746D818F4f239e57986d810a4d55526526414C5D"; // place your public address for your wallet here
+const walletAddress = "0x28D1759B13CD4fA182d18E2Be3D4A5DdBEE6920F"; // place your public address for your wallet here
 
 async function main() {
 
